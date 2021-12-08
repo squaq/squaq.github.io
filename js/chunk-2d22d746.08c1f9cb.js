23c82b1c4f86d0168af4a702968ba207e726c3f3
Everton Melo <e859043@corp.santander.pt>
2021-12-08 23:59:58 +0000

    fonts

--- js/chunk-2d22d746.08c1f9cb.js
+++ js/chunk-2d22d746.08c1f9cb.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("Preloader")],1)},l=[],o=t("f152"),r={components:{Preloader:o["a"]}},s=r,c=t("2877"),u=Object(c["a"])(s,a,l,!1,null,null,null);n["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d22d746.b077b3c5.js.map
+//# sourceMappingURL=chunk-2d22d746.08c1f9cb.js.map
